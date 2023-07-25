8c338eb2b690056e3d88a1146d941350b0ace597
NaelShichida <nael.shichida2@gmail.com>
2023-07-25 17:14:24 +0100

    Etherjs deployment

--- deploy.js
+++ deploy.js
@@ -1,21 +1,27 @@
 const ethers = require("ethers");
-const fs = require("fs-extra")
+const fs = require("fs-extra");
 
 async function main() {
-    // http://127.0.0.1:7545
-    const provider = new ethers.JsonRpcProvider("http://127.0.0.1:7545")
-    const wallet = new ethers.Wallet("0x19e67d6c95eadf31b63df31bca5098b52c481bd97edd8db3e5645a65691610e8",provider)
-    const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi","utf8")
-    const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin","utf8");
-    const contractFactory = new ethers.ContractFactory(abi,binary,wallet);
-    console.log("Deploying, please wait....")
-    const contract = await contractFactory.deploy();
-    console.log(contract)
+  // http://127.0.0.1:7545
+  const provider = new ethers.JsonRpcProvider("http://127.0.0.1:7545");
+  const wallet = new ethers.Wallet(
+    "0x19e67d6c95eadf31b63df31bca5098b52c481bd97edd8db3e5645a65691610e8",
+    provider
+  );
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying, please wait....");
+  const contract = await contractFactory.deploy();
+  console.log(contract);
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error)
-        process.exit(1)
-    })
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
