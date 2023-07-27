ac6fa6dae794ccff54f6bad1924c39f67e2f0d3b
NaelShichida <nael.shichida2@gmail.com>
2023-07-27 15:07:54 +0100

    Deploying to Sepolia Testnet & downgraded ether js

--- deploy.js
+++ deploy.js
@@ -1,42 +1,31 @@
-const ethers = require("ethers");
-const fs = require("fs-extra");
-require("dotenv").config();
+const ethers = require("ethers")
+const fs = require("fs-extra")
+require("dotenv").config()
 
 async function main() {
-  const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
-  const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
+    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
 
-  // let wallet = new ethers.Wallet.fromEncryptedJsonSync(
-  //   encryptedJson,
-  //   process.env.PRIVATE_KEY_PASSWORD
-  // ); //this is no longer viable This is because fromEncryptedJsonSync is not a constructor function, but rather a static method on the Wallet class that is used to create a wallet instance from an encrypted JSON keystore.
+    const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8")
+    const binary = fs.readFileSync(
+        "./SimpleStorage_sol_SimpleStorage.bin",
+        "utf8",
+    )
 
-  const wallet = await ethers.Wallet.fromEncryptedJsonSync(
-    encryptedJson,
-    process.env.PRIVATE_KEY_PASSWORD
-  );
+    const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
+    console.log("Deploying, please wait....")
 
-  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
-  const binary = fs.readFileSync(
-    "./SimpleStorage_sol_SimpleStorage.bin",
-    "utf8"
-  );
+    const contract = await contractFactory.deploy()
+    // await contract.deploymentTransaction.wait(1) //for some reason this prevents the deployment, perhaps two awaits dont work in tandem?
+    console.log(`Contract deployed to ${contract.address}`)
 
-  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
-  console.log("Deploying, please wait....");
-
-  const contract = await contractFactory.deploy();
-  console.log(contract);
-
-  await contract.deploymentTransaction.wait(1);
-
-  const currentFavouriteNumber = await contract.retrieve();
-  console.log(currentFavouriteNumber);
+    const currentFavouriteNumber = await contract.retrieve()
+    console.log(currentFavouriteNumber)
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
